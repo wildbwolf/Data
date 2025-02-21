01f06df1065e3cea7f680d3ff5da03f2e7094840
wildbwolf <flyphoenix31@gmail.com>
2025-02-21 09:52:29 -0600

    updated credentials

---
+++ Premier/Credential.ts
@@ -0,0 +1,24 @@
+/***
+ * GAV Mail : ted.kwong@gavstech.com
+ * pwd : Time2M@keM0ney!@#
+ */
+   
+/***
+ * Premier Mail : ted_kwong@premierinc.com
+ * pwd : Time2M@keM0ney!
+ */
+
+/***
+ * Beeline Credential
+ * Beeline link : https://prod.beeline.com/premier/welcome/
+ * email : tedkwong338@gmail.com
+ * pwd : Time2M@keM0ney!!
+ */
+
+/***
+ * 365 Microsoft
+ * Link : https://login.microsoftonline.com/
+ * https://m365.cloud.microsoft/
+ * email : ted.kwong@gavstech.com
+ * pwd : Time2M@keM0ney!
+ */
